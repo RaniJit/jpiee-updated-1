e49f52b762835c8d67dbc835cb1ee3178b55e398
ShawnFrost26 <sb262202@gmail.com>
2024-02-09 00:05:42 +0530

    fixed menu close clicking outside

--- frontend/modules/menuClose.js
+++ frontend/modules/menuClose.js
@@ -1,18 +1,32 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var menuButton = document.getElementById("menuButton");
+//   var mobileMenu = document.querySelector(".mobile-menu");
+
+//   // Open/close mobile menu on button click
+//   menuButton.addEventListener("click", function () {
+//     mobileMenu.classList.toggle("show");
+//   });
+
+//   // Close mobile menu when clicking outside the menu
+//   document.addEventListener("click", function (event) {
+//     var isClickInsideMobileMenu = mobileMenu.contains(event.target);
+//     var isClickOnMenuButton = menuButton.contains(event.target);
+
+//     if (!isClickInsideMobileMenu && !isClickOnMenuButton) {
+//       mobileMenu.classList.remove("show");
+//     }
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   var menuButton = document.getElementById("menuButton");
   var mobileMenu = document.querySelector(".mobile-menu");
 
-  // Open/close mobile menu on button click
-  menuButton.addEventListener("click", function () {
-    mobileMenu.classList.toggle("show");
-  });
-
   // Close mobile menu when clicking outside the menu
   document.addEventListener("click", function (event) {
-    var isClickInsideMobileMenu = mobileMenu.contains(event.target);
     var isClickOnMenuButton = menuButton.contains(event.target);
-
-    if (!isClickInsideMobileMenu && !isClickOnMenuButton) {
+    // Check if the click is outside the menu and not on the menu button
+    if (!isClickOnMenuButton) {
       mobileMenu.classList.remove("show");
     }
   });
